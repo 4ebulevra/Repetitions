6fb8266a004f737c83a73b3b287c406c04e45378
4ebulevra <4ebulevra@gmail.com>
2021-11-21 00:09:23 +0700

    Решена задача № 6

--- [GB] CSHARP 01 HomeWork Console/Task06.cs
+++ [GB] CSHARP 01 HomeWork Console/Task06.cs
@@ -8,14 +8,31 @@ namespace _GB__CSHARP_01_HomeWork_Console
 {
     class Task06 : Task
     {
+        private int number;
+        public Task06()
+        {
+            task = "Написать метод подсчета количества цифр числа.\n" +
+                   "-------------------------------------------------------------------------------";
+        }
         private void CollectData()
         {
+            while (true)
+            {
+                Console.Write("Введитите целое положительное число: ");
+                if (int.TryParse(Console.ReadLine(), out number)) if (number > 0) break;
+            }
 
+        }
+
+        private int Nums(int x)
+        {
+            if (x < 10) return 1;
+            return 1 + Nums(x / 10);
         }
 
         private void PrintData()
         {
-
+            Console.Write($"В заданном числе знаков: {Nums(number)}");
         }
 
         public override void Start()
