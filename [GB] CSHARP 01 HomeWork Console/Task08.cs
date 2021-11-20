844b666a88ad5584c2c01cb9413b41027fb0b33b
4ebulevra <4ebulevra@gmail.com>
2021-11-21 01:16:12 +0700

    Решена задача № 8

--- [GB] CSHARP 01 HomeWork Console/Task08.cs
+++ [GB] CSHARP 01 HomeWork Console/Task08.cs
@@ -6,16 +6,41 @@ using System.Threading.Tasks;
 
 namespace _GB__CSHARP_01_HomeWork_Console
 {
-    class Task08: Task
+    class Task08 : Task
     {
-        private void CollectData()
+        private string login;
+        private string pass;
+        private int tryAuth;
+        private bool passed;
+        public Task08()
         {
-
+            passed = false;
+            tryAuth = 3;
+            task = "Реализовать метод проверки логина и пароля. На вход метода подается логин и пароль. " +
+                   "На выходе истина, если прошел авторизацию, и ложь, если не прошел(Логин: root, Password: GeekBrains).\n" +
+                   "Используя метод проверки логина и пароля,написать программу: пользователь вводит логин и пароль, " +
+                   "программа пропускает его дальше или не пропускает. С помощью цикла do while ограничить ввод пароля тремя попытками." +
+                   "-------------------------------------------------------------------------------";
         }
+
+        private bool CheckAuth(string _login, string _pass)
+        {
+            if (_login == "root" && _pass == "GeekBrains") return true;
+
+            return false;
+        }
+
 
         private void PrintData()
         {
-
+            if (passed)
+            {
+                Console.WriteLine("Данные для входа верны!");
+            }
+            else
+            {
+                Console.WriteLine("Данные для входа НЕверны!");
+            }
         }
 
         public override void Start()
@@ -23,7 +48,22 @@ namespace _GB__CSHARP_01_HomeWork_Console
             Console.Clear();
             Console.Title = "Task08";
             Console.WriteLine($"Задание: {task}");
-            CollectData();
+
+            
+            do
+            {
+                Console.Write("Введите логин: ");
+                login = Console.ReadLine();
+                Console.Write("Введите пароль: ");
+                pass = Console.ReadLine();
+                if (CheckAuth(login, pass))
+                {
+                    passed = true;
+                    break;
+                }
+                tryAuth = tryAuth - 1;
+            } while (tryAuth > 0);
+
             PrintData();
         }
     }
