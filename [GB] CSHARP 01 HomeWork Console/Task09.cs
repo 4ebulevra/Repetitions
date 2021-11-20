c39b60b87ae74d43372f3dcd1dd602334ceea333
4ebulevra <4ebulevra@gmail.com>
2021-11-21 01:41:37 +0700

    в процессе решения задачи № 9

--- [GB] CSHARP 01 HomeWork Console/Task09.cs
+++ [GB] CSHARP 01 HomeWork Console/Task09.cs
@@ -8,14 +8,31 @@ namespace _GB__CSHARP_01_HomeWork_Console
 {
     class Task09 : Task
     {
+        private double Weight;
+        private double Height;
+        private int IMT;
+
+        public Task09()
+        {
+            task = "Написать программу, которая запрашивает массу и рост человека, вычисляет его индекс массы и сообщает, " +
+                   "нужно ли человеку похудеть, набрать вес или всё в норме.\n" +
+                   "*Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.\n" +
+                   "-------------------------------------------------------------------------------";
+        }
         private void CollectData()
         {
+            Console.Write("Введите вес(кг): ");
+            Weight = double.Parse(Console.ReadLine());
 
+            Console.Write("Введите рост(м): ");
+            Height = double.Parse(Console.ReadLine());
+
+            IMT = (int) (Weight / (Height * Height));
         }
 
         private void PrintData()
         {
-
+            Console.WriteLine(IMT);
         }
 
         public override void Start()
