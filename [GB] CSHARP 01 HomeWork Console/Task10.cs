2c6335176338ad95106dc54de6c58f4c537e9175
4ebulevra <4ebulevra@gmail.com>
2021-11-21 20:16:09 +0700

    Решены задачи 10 и 11

--- [GB] CSHARP 01 HomeWork Console/Task10.cs
+++ [GB] CSHARP 01 HomeWork Console/Task10.cs
@@ -8,14 +8,61 @@ namespace _GB__CSHARP_01_HomeWork_Console
 {
     class Task10 : Task
     {
-        private void CollectData()
+        private int count;
+        public Task10()
         {
+            count = 0;
+            task = "Написать программу подсчета количества «хороших» чисел в диапазоне от 1 до 1 000 000 000. " +
+                   "«Хорошим» называется число, которое делится на сумму своих цифр.\n" +
+                   "*Реализовать подсчёт времени выполнения программы, используя структуру DateTime.\n" +
+                   "-------------------------------------------------------------------------------";
+        }
+
+        private bool CheckGoodNum(int x)
+        {
+            int num = x;
+            int sum = 0;
+            while (true)
+            {
+                if (x <= 0)
+                {
+                    break;
+                }
+                else if (x > 10)
+                {
+                    sum = sum + x % 10;
+                    x = x / 10;
+                }
+                else
+                {
+                    sum = sum + x;
+                    break;
+                }
+            }
+            if (sum > 0 && (num % sum) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
 
         }
 
         private void PrintData()
         {
-
+            Console.Write("Введите верхний диапазон поиска хороших чисел: ");
+            int maxValue = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Производится поиск хороших чисел в диапазоне от 1 до {maxValue}...");
+            var a = DateTime.Now;
+            for (int i = 1; i < maxValue; i++)
+            {
+                if (CheckGoodNum(i)) count++;
+            }
+            var b = DateTime.Now;
+            Console.WriteLine($"Найдено хороших чисел: {count}\n" +
+                              $"Затрачено времени: {(b - a).ToString()}");
         }
 
         public override void Start()
@@ -23,7 +70,6 @@ namespace _GB__CSHARP_01_HomeWork_Console
             Console.Clear();
             Console.Title = "Task10";
             Console.WriteLine($"Задание: {task}");
-            CollectData();
             PrintData();
         }
     }
--- [GB] CSHARP 01 HomeWork Console/Task11.cs
+++ [GB] CSHARP 01 HomeWork Console/Task11.cs
@@ -6,16 +6,34 @@ using System.Threading.Tasks;
 
 namespace _GB__CSHARP_01_HomeWork_Console
 {
-    class Task11 :Task
+    class Task11 : Task
     {
-        private void CollectData()
+        private int a;
+        private int b;
+        public Task11()
         {
-
+            task = "Разработать рекурсивный метод, который выводит на экран числа от a до b(a<b).\n" +
+                   "*Разработать рекурсивный метод, который считает сумму чисел от a до b.\n" +
+                   "-------------------------------------------------------------------------------";
         }
 
-        private void PrintData()
+        private void CollectData()
         {
+            Console.Write("Введите число а: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Введите число b: ");
+            b = int.Parse(Console.ReadLine());
+        }
 
+        private void PrintList(int x, int y)
+        {
+            Console.WriteLine(x);
+            if (x < y) PrintList(x + 1, y);
+        }
+
+        private int Sum(int x, int y)
+        {
+            return y == x ? y : Sum(x, y - 1) + y;
         }
 
         public override void Start()
@@ -24,7 +42,10 @@ namespace _GB__CSHARP_01_HomeWork_Console
             Console.Title = "Task11";
             Console.WriteLine($"Задание: {task}");
             CollectData();
-            PrintData();
+            Console.WriteLine($"Сумма чисел в диапазоне: {Sum(a < b ? a : b, b > a ? b : a)}");
+            Console.Write("Нажмите Enter, что бы вывести список чисел в диапазоне...");
+            Console.ReadLine();
+            PrintList(a < b ? a : b, b > a ? b : a);
         }
     }
 }
