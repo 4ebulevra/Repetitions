3f5c31d132a6760170b993f062bfbebbb9a35202
4ebulevra <4ebulevra@gmail.com>
2021-11-24 23:10:16 +0700

    Начато задание 12, для 13-15 заданий созданы классы

---
+++ [GB] CSHARP 01 HomeWork Console/Task12.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _GB__CSHARP_01_HomeWork_Console
+{
+    class Task12 : Task
+    {
+        public Task12()
+        {
+            task = "С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке). " +
+                   "Требуется подсчитать сумму всех нечётных положительных чисел. " +
+                   "Сами числа и сумму вывести на экран, используя tryParse.\n" +
+                   "-------------------------------------------------------------------------------";
+        }
+
+        private void CollectData()
+        {
+            Console.Write("Введите цело положительное число или 0 для подсчёта суммы введёных нечётных чисел: ");
+        }
+
+        private void PrintData()
+        {
+
+        }
+
+        public override void Start()
+        {
+            Console.Clear();
+            Console.Title = "Task12";
+            Console.WriteLine($"Задание: {task}");
+            CollectData();
+            PrintData();
+
+        }
+    }
+}
