1da527b2a142b2cd9185e6d162aa9e1beca3b10c
4ebulevra <4ebulevra@gmail.com>
2022-01-09 03:43:40 +0700

    Добавлены классы и описания для задач 14 - 18

---
+++ [GB] CSHARP 01 HomeWork Console/Task17.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _GB__CSHARP_01_HomeWork_Console
+{
+    internal class Task17 : Task
+    {
+        public Task17()
+        {
+            task = "Решить раннюю задачу с логинами, только логины и пароли считать из файла в массив. " +
+                   "Создайте структуру Account, содержащую Login и Password.\n" +
+                   "-------------------------------------------------------------------------------";
+        }
+
+        private void CollectData()
+        {
+
+        }
+
+        private void PrintData()
+        {
+
+        }
+
+        public override void Start()
+        {
+            Console.Clear();
+            Console.Title = "Task17";
+            Console.WriteLine($"Задание: {task}");
+            CollectData();
+            PrintData();
+        }
+    }
+}
